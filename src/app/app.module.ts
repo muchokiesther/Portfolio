c9941af9d189bf3a8da6c76e89747240178f34e3
muchokiesther <muchokiesther8@gmail.com>
2023-07-10 16:11:13 +0300

    This are changes on the header componnet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderComponent,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
